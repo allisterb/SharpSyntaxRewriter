204efc865f29fb8c050856509db75ffca54756f4
Allister Beharry <allister.beharry@gmail.com>
2022-01-28 18:04:57 -0400

    Add NoNameof rewriter.

---
+++ src/SharpSyntaxRewriter/Rewriters/NoNameof.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+using SharpSyntaxRewriter.Adapters;
+using SharpSyntaxRewriter.Rewriters.Types;
+using SharpSyntaxRewriter.Utilities;
+
+namespace SharpSyntaxRewriter.Rewriters
+{
+    public class NoNameof : SymbolicRewriter
+    {
+        public const string ID = "<no nameof>";
+
+        public override string Name()
+        {
+            return ID;
+        }
+
+        public override SyntaxNode VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            if (!node.ToFullString().StartsWith("nameof(", StringComparison.InvariantCulture)) return base.VisitInvocationExpression(node);
+            var arg = node
+                .DescendantNodes()
+                .OfType<ArgumentSyntax>()
+                .First().DescendantNodes()
+                .OfType<IdentifierNameSyntax>()
+                .First();
+            return SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(arg.ToFullString()));
+        }
+    }
+}
